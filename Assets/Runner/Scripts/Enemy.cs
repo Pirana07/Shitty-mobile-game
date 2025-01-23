b544a9c046541a0cc2fc67fd4776b5888a3218a2
Pirana07 <pirananika58@gmail.com>
2025-01-23 14:49:55 +0400

    To be continued...
    Making enemy logic

--- Assets/Runner/Scripts/Enemy.cs
+++ Assets/Runner/Scripts/Enemy.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    enum State{ Idle, Running }
+
+    [Header("Settings")]
+    [SerializeField] float searchRadius;
+    [SerializeField] float moveSpeed;
+    State state;
+    Transform targetRunner;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,46 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ManageSate();
         
+    }
+    void ManageSate(){
+        switch(state){
+            case State.Idle:
+                SearchForTarget();
+             break;
+             case State.Running:
+                RunTorwardsTargt();
+              break;
+        }
+    }
+    void SearchForTarget(){
+        Collider[] detectedColliders = Physics.OverlapSphere(transform.position, searchRadius);
+        for (int i = 0; i < detectedColliders.Length; i++)
+        {
+            if(detectedColliders[i].TryGetComponent(out targetRunner runner)){
+                if(runner.IsTarget()){
+                    continue;
+                }
+                runner.SetTarget();
+                targetRunner = runner;
+
+                StartRunningTorwardsTarget();
+            }
+        }
+    }
+    void StartRunningTorwardsTarget(){
+        state = State.Running;
+        GetComponent<Animator>().Play("Run");
+    }
+    void RunTorwardsTarget(){
+        if(targetRunner == null){
+            return;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, targetRunner.position, Time.deltaTime * moveSpeed);
+        if(Vector3.Distance(transform.position, targetRunner.position) > .1f){
+            Destroy(targetRunner.gameObject);
+            Destroy(gameObject);
+        }
     }
 }
