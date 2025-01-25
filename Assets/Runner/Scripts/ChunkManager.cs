f744919bb1ccc45b9c4a885afb8def1c8a6982dc
Pirana07 <pirananika58@gmail.com>
2025-01-25 17:28:10 +0400

    ...

--- Assets/Runner/Scripts/ChunkManager.cs
+++ Assets/Runner/Scripts/ChunkManager.cs
@@ -5,67 +5,63 @@ using UnityEngine;
 public class ChunkManager : MonoBehaviour
 {
     public static ChunkManager instance;
-    
+
     [Header("Elements")]
     [SerializeField] LevelSO[] levels;
     GameObject finishLine;
 
     [Header("Settings")]
-    [SerializeField]  int numberOfChunks = 5; 
-    [SerializeField]  Vector3 chunkStartOffset = Vector3.zero; 
-    
-     void Awake(){
-        if(instance != null){
+    [SerializeField] int numberOfChunks = 5;
+    [SerializeField] Vector3 chunkStartOffset = Vector3.zero;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
             Destroy(gameObject);
-        }else{
+        }
+        else
+        {
             instance = this;
         }
     }
 
     void Start()
-    { 
+    {
         finishLine = GameObject.FindWithTag("Finish");
+        GenerateLevel();
     }
-    void GenerateLevel(){
+
+    void GenerateLevel()
+    {
         int currentLevel = GetLevel();
         currentLevel = currentLevel % levels.Length;
         LevelSO level = levels[currentLevel];
-
         CreateLevel(level.chunks);
     }
 
-    void CreateLevel(Chunk[] levelChunks){
-        Vector3 chunkPos = chunkStartOffset; 
-        for (int i = 0; i < levelChunks.Length; i++){
-            Chunk chunkToCreate = levelChunks[i];
-             if(i > 0){
-                chunkPos.z += chunkToCreate.GetLength() / 2;
-             }
-             
-            Chunk chunkInstance = Instantiate(chunkToCreate, chunkPos, Quaternion.identity, transform); 
-            chunkPos.z += chunkInstance.GetLength() / 2;
-        }
-
-    }
-    /*
-    void CreateRandomLevel(){
-        Vector3 chunkPos = chunkStartOffset; 
-
-        for (int i = 0; i < numberOfChunks; i++)
+    void CreateLevel(Chunk[] levelChunks)
+    {
+        Vector3 chunkPos = chunkStartOffset;
+        for (int i = 0; i < levelChunks.Length; i++)
         {
-             Chunk chunkToCreate = chunksPrefabs[Random.Range(0, chunksPrefabs.Length)];
-             if(i > 0){
+            Chunk chunkToCreate = levelChunks[i];
+            if (i > 0)
+            {
                 chunkPos.z += chunkToCreate.GetLength() / 2;
-             }
-            Chunk chunkInstance = Instantiate(chunkToCreate, chunkPos, Quaternion.identity, transform); 
+            }
+            Chunk chunkInstance = Instantiate(chunkToCreate, chunkPos, Quaternion.identity, transform);
             chunkPos.z += chunkInstance.GetLength() / 2;
         }
     }
-    */
-    public float GetFinishZ(){
+
+    public float GetFinishZ()
+    {
         return finishLine.transform.position.z;
     }
-    public int GetLevel(){
+
+    public int GetLevel()
+    {
         return PlayerPrefs.GetInt("level", 0);
     }
 }
